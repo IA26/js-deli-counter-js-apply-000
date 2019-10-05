904d16e6b596730d8a10943d6d1b2d4c6eb5ee31
Isaac Avilez <isaac1180@aol.com>
2019-10-05 12:56:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,41 +2,6 @@
 
 function takeANumber(currentLine, personsName) {
   currentLine.push(personsName);
-  var str = "Welcome " + personsName + ". You are number " + currentLine.length +  " in line." ;
-  return str;
-}
-
-function nowServing(currentLine) {
-  if (currentLine.length === 0)
-  // Checks if currentLine is empty (no elements in array)
-    return "There is nobody waiting to be served";
-  else 
-  {
-    var name = currentLine[0];
-    currentLine.shift();
-    return name;
-  }
-}
-
-function currentLine(currentLine) {
-  if (currentLine.length === 0)
-    return "The line is currently empty";
-  else {
-    var str = "The line is currently: ";
-    for (var i = 0; i < currentLine.length ; i++)
-    {
-      str = str + (i+1) + ". " + currentLine[i];
-      if (i < currentLine.length - 1)
-        str = str + ", ";
-    }
-    return str;
-  }
-}
-
-
-
-function takeANumber(currentLine, personsName) {
-  currentLine.push(personsName);
   var str = "Welcome, " + personsName + ". You are number " + currentLine.length +  " in line." ;
   return str;
 }
@@ -69,3 +34,18 @@ function currentLine(currentLine) {
 
 var katzDeliLine = [];
  
+ ticketNumber(katzDeliLine,  
+ 
+ 
+ 
+ function takeANumber(currentLine) {
+   ticketNumber++
+  currentLine.push(ticketNumber);
+  var str = "Welcome, " + ticketNumber + ". You are number " + currentLine.length +  " in line." ;
+  return str;
+}
+
+var ticketNumber = 0;
+
+
+
